3bff2933a92db117668cead6bd2797fe6f4d9661
Adam Goodnight <chaofoam@gmail.com>
2025-04-11 14:36:57 -0400

    answers are now sorted by thier question

--- main.js
+++ main.js
@@ -1,29 +1,22 @@
 const { program } = require('commander');
-const questions = require('./questions.json');
+const questions = require('./questions.test.json');
 const fs = require('fs')
 const { default: inquirer } = require('inquirer');
 const moment = require('moment');
+const chalk = require('chalk');
 
-const getQuestion = () => {
+function getQuestion() {
     return questions.questions[
         Math.floor(
             Math.random() * questions.questions.length)
     ]
 }
 
-const captureAnswer = (message, answer) => {
-    const now = moment(Date.now()).format('MMMM Do YYYY, h:mm:ss a')
-    return `
-    "${now}":[
-        {
-            "${message}": "${answer.response}"\n
-        }
-    ]`
-}
-
-program.version("0.0.1").description("A simple cli tool to answer interview questions")
+program.version("0.0.1")
+    .description("A simple cli tool to answer interview questions")
     .action(async () => {
 
+        const now = moment(Date.now()).format('MMMM Do YYYY, h:mm:ss a')
         const message = getQuestion();
         const output = 'answers.json';
 
@@ -34,27 +27,33 @@ program.version("0.0.1").description("A simple cli tool to answer interview ques
                 message
             }
         );
+        const newAnswer = {
+            answer: answer.response,
+            date: now
+        }
 
         if (fs.existsSync(output)) {
             const contents = fs.readFileSync(output)
-            previous = JSON.stringify(JSON.parse(contents), null, '\t')
-            // console.log(previous[0], previous[previous.length - 1])
-            rest = previous.slice(1, previous.length - 1)
-            console.log(rest)
-            captureAnswer(message, answer, output)
-            next = (rest.length > 0) ? `{
-    ${captureAnswer(message, answer, output)},
-    ${rest}
-}` : `{
-    ${captureAnswer(message, answer, output)}
-}`
-            fs.writeFileSync(output, next)
-        } else {
-            fs.writeFileSync(output, `{
-    ${captureAnswer(message, answer, output)}
-}`)
-        }
+            const previousJson = JSON.parse(contents);
+            const jsonCopy = { ...previousJson };
 
+            const existingAnswers = Object.keys(previousJson).find((question) => question === message);
+            if (existingAnswers) {
+                console.log(chalk.blueBright(`You've answered this question ${jsonCopy[existingAnswers].length} times`))
+                jsonCopy[existingAnswers].push(newAnswer)
+            } else {
+                console.log(chalk.blueBright(`First time answering this question`))
+                jsonCopy[message] = [newAnswer]
+            }
+
+            fs.writeFileSync(output, JSON.stringify(jsonCopy, null, '\t'))
+        } else {
+            const newQA = {
+                [message]: [newAnswer]
+            }
+            console.log(chalk.blueBright(`This is the first question you've ever answered!`));
+            fs.writeFileSync(output, JSON.stringify(newQA, null, '\t'))
+        }
     })
 
 program.parse(process.argv)
